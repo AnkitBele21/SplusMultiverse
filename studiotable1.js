9eb6defa8960a2ca0c3d5ba4fef21e30ea36925c
AnkitBele21 <147297226+AnkitBele21@users.noreply.github.com>
2023-12-05 12:07:18 +0530

    Create studiotable1.js

---
+++ studiotable1.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetchDataAndUpdateDisplay();
+});
+
+function fetchDataAndUpdateDisplay() {
+    // Fetch data specific to Table 1
+    // Process and update the display
+}

834c931ff8c29938e8375bdf814d69b826e749f6
AnkitBele21 <147297226+AnkitBele21@users.noreply.github.com>
2023-12-10 21:33:51 +0530

    Update studiotable1.js

--- studiotable1.js
+++ studiotable1.js
@@ -1,6 +1,6 @@
-const API_KEY = 'AIzaSyCfxg14LyZ1hrs18WHUuGOnSaJ_IJEtDQc'; // Caution: Exposing API key
+const API_KEY = 'AIzaSyCfxg14LyZ1hrs18WHUuGOnSaJ_IJEtDQc';
 const SHEET_ID = '1Bcl1EVN-7mXUP7M1FL9TBB5v4O4AFxGTVB6PwqOn9ss';
-const SHEET_NAME = 'af'; // Sheet name for match details
+const SHEET_NAME = 'af';
 
 document.addEventListener('DOMContentLoaded', function() {
     fetchDataAndUpdateDisplay();
@@ -11,19 +11,14 @@ function fetchDataAndUpdateDisplay() {
 
     fetch(url)
         .then(response => response.json())
-        .then(data => {
-            processData(data.values);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-            displayNotActive();
-        });
+        .then(data => processData(data.values))
+        .catch(error => console.error('Error fetching data:', error));
 }
 
 function processData(data) {
-    const rowData = data[1]; // Get the second row (index 1) directly
+    const rowData = data[1];
 
-    if (rowData && rowData.length >= 10) { // Check if rowData has enough elements
+    if (rowData && rowData.length >= 10) {
         displayMatchDetails(rowData);
     } else {
         displayNotActive();
@@ -32,37 +27,34 @@ function processData(data) {
 
 function displayMatchDetails(rowData) {
     const logoContainer = document.getElementById('logo-container');
-    logoContainer.className = 'logo-active'; // Smaller logo for active match
+    logoContainer.className = 'logo-active';
 
     const matchInfoElement = document.getElementById('match-info');
-    if (matchInfoElement) {
-        matchInfoElement.innerHTML = `
-            <div class="row justify-content-center">
-                <div class="col-md-4 player-card">
-                    <div class="player-name">${rowData[2]}</div>
-                    <div class="player-details">Rank: ${rowData[4]}, Level: ${rowData[5]}, Win Rate: ${rowData[8]}%</div>
-                </div>
-                <div class="col-md-2 d-flex align-items-center justify-content-center">
-                    <div class="vs-text">VS</div>
-                </div>
-                <div class="col-md-4 player-card">
-                    <div class="player-name">${rowData[3]}</div>
-                    <div class="player-details">Rank: ${rowData[6]}, Level: ${rowData[7]}, Win Rate: ${rowData[9]}%</div>
-                </div>
+    matchInfoElement.innerHTML = `
+        <div class="row justify-content-center">
+            <div class="col-md-4 player-card">
+                <div class="player-name">${rowData[2]}</div>
+                <div class="player-details">Rank: ${rowData[4]}, Level: ${rowData[5]}, Win Rate: ${rowData[8]}%</div>
             </div>
-            <div class="row justify-content-center">
-                <div class="col-md-12">
-                    <div class="start-time">Start Time: ${rowData[1]}</div>
-                </div>
+            <div class="col-md-2 d-flex align-items-center justify-content-center">
+                <div class="vs-text">VS</div>
             </div>
-        `;
-    }
+            <div class="col-md-4 player-card">
+                <div class="player-name">${rowData[3]}</div>
+                <div class="player-details">Rank: ${rowData[6]}, Level: ${rowData[7]}, Win Rate: ${rowData[9]}%</div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="start-time">Start Time: ${rowData[1]}</div>
+            </div>
+        </div>
+    `;
 }
 
 function displayNotActive() {
     const logoContainer = document.getElementById('logo-container');
-    logoContainer.className = 'logo-inactive'; // Larger logo for inactive state
-
+    logoContainer.className = 'logo-inactive';
     const matchInfoElement = document.getElementById('match-info');
-    matchInfoElement.innerHTML = '';
+    matchInfoElement.innerHTML = '<p>No Active Matches</p>';
 }

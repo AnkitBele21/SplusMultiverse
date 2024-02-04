a6aa10d412ae355b32da46a01089bb59ceba0d90
AnkitBele21 <147297226+AnkitBele21@users.noreply.github.com>
2024-02-04 21:43:21 +0530

    Update datapull.js

--- datapull.js
+++ datapull.js
@@ -54,7 +54,7 @@ function createPlayerCard(player) {
     // Add "Playing at Club" above S+ Coins if the status is "Playing Now"
     if (status && status.toLowerCase() === 'playing now') {
         const playingAtClub = document.createElement('span');
-        playingAtClub.textContent = 'Playing at Club';
+        playingAtClub.textContent = 'Playing at Studio ';
         playingAtClub.className = 'playing-at-club';
         playerInfo.appendChild(playingAtClub);
 

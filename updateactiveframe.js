8b9c88d557c792fd0ff22a7990795913b044011e
AnkitBele21 <147297226+AnkitBele21@users.noreply.github.com>
2024-04-01 17:33:08 +0530

    Update updateactiveframe.js

--- updateactiveframe.js
+++ updateactiveframe.js
@@ -69,15 +69,13 @@ function prefillForm(rowData, frameId) {
 // Function to get the current timestamp in the required format
 function getCurrentTimestamp() {
     const now = new Date();
-    const year = now.getFullYear();
-    const month = (now.getMonth() + 1).toString().padStart(2, '0');
-    const day = now.getDate().toString().padStart(2, '0');
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
 
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return `${hours}:${minutes}:${seconds}`;
 }
+
 
 // Function to update the startTime in the Google Sheets document
 async function updateStartTimeInSheet(frameId, startTime) {
